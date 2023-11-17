ac5f936bfcd5b1c8d355de8384260db0cbc1b48a
Luc W <lucw@airpost.net>
2023-11-17 21:44:26 +0800

    add possible translation

--- manual_test_postdeploy.py
+++ manual_test_postdeploy.py
@@ -154,7 +154,8 @@ class PostDeployTests(unittest.TestCase):
         }, headers={'api_key': self.api_key})
 
         data = response.json()
-        possible_translations = ['à bas prix', 'Faible coût', 'À bas prix', 'faible coût', 'très faible coût', 'Le coût est très bas', 'Le coût est très faible']
+        possible_translations = ['à bas prix', 'Faible coût', 'À bas prix', 'faible coût', 'très faible coût', 'Le coût est très bas', 'Le coût est très faible',
+            'Très faible coût']
         self.assertTrue(data['Azure'] == 'Le coût est faible' or data['Azure'] == 'Le coût est très faible')
         self.assertIn(data['Amazon'], possible_translations)
         self.assertIn(data['Google'], possible_translations)
