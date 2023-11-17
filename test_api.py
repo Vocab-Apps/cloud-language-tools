14d2e5afcc70be4fd1573acf67c2b63d4e7454db
Luc W <lucw@airpost.net>
2023-11-17 21:41:39 +0800

    add possible translation

--- test_api.py
+++ test_api.py
@@ -232,7 +232,7 @@ class ApiTests(unittest.TestCase):
         data = json.loads(response.data)
         self.assertTrue(data['Azure'] == 'Le coût est faible' or data['Azure'] == 'Le coût est très faible')
         self.assertIn(data['Amazon'], ['Très faible coût', 'Le coût est très faible', 'faible coût', 'Le coût est très bas'])
-        self.assertIn(data['Google'], ['à bas prix', 'Faible coût', 'À bas prix', 'faible coût', 'très faible coût', 'Le coût est très faible'])
+        self.assertIn(data['Google'], ['à bas prix', 'Faible coût', 'À bas prix', 'faible coût', 'très faible coût', 'Le coût est très faible', 'Très faible coût'])
         self.assertEqual(data['Watson'], 'Le coût est très bas.')        
 
 
