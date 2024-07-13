ad2aee823f683cc09ca8919f431038b0018846dd
Luc W <lucw@airpost.net>
2024-07-13 20:02:44 +0800

    add max requests and max requests jitter

--- start.sh
+++ start.sh
@@ -23,6 +23,8 @@ else
 . ${CWD}/convertkit.sh
 WORKERS="${GUNICORN_WORKERS:-1}" 
 GUNICORN_THREADS="${GUNICORN_THREADS:-8}"
-echo "starting gunicorn with ${WORKERS} workers" 
-exec gunicorn --workers $WORKERS --threads ${GUNICORN_THREADS} -b :8042 --timeout 120 --access-logfile - --error-logfile - app:app
+MAX_REQUESTS="${GUNICORN_MAX_REQUESTS:50000}" # should amount to a weekly restart
+MAX_REQUESTS_JITTER="${GUNICORN_MAX_REQUESTS_JITTER:5000}"
+echo "starting gunicorn with ${WORKERS} workers, ${GUNICORN_THREADS} threads, and ${MAX_REQUESTS} max requests, max requests jitter ${MAX_REQUESTS_JITTER}" 
+exec gunicorn --workers $WORKERS --threads ${GUNICORN_THREADS} -b :8042 --timeout 120 --max-requests ${MAX_REQUESTS} --max-requests-jitter ${MAX_REQUESTS_JITTER} --access-logfile - --error-logfile - app:app
 fi
